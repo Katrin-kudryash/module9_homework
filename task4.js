7c15ffb53fb0b36f8b391f5c80675e64262db8f9
Katrinputrina <putrina.katya@gmail.ru>
2022-01-04 19:00:11 +0300

    task4: Добавлено условие и решение задачи

---
+++ task4.js
@@ -0,0 +1,35 @@
+// Задание 4.
+// Напишите код приложения, интерфейс которого представляет собой 2 input и кнопку submit.
+// В input можно ввести любое число. При клике на кнопку происходит следующее:
+
+// Если оба числа не попадают в диапазон от 100 до 300 или 
+// введено не число — выводить ниже текст «одно из чисел вне диапазона от 100 до 300»;
+// Если числа попадают в диапазон от 100 до 300 — сделать запрос 
+// c помощью fetch по URL https://picsum.photos/200/300, где первое число — ширина картинки, второе — высота.
+
+
+// Получаем элементы
+const buttonNode = document.querySelector('.button');
+const resultNode = document.querySelector('.output');
+
+
+buttonNode.addEventListener('click', () => {
+	const widthPicture = document.getElementById('width-picture').value;
+	const heightPicture = document.getElementById('height-picture').value;
+		if (widthPicture < 100 || widthPicture > 300 || heightPicture < 100 || heightPicture > 300) {
+			resultNode.innerHTML = "<span>Одно из чисел вне диапазона от 100 до 300</span>"
+		} else {
+			fetch(`https://picsum.photos/${widthPicture}/${heightPicture}`)
+				.then((response) => {
+					const cardBlock = `
+					<div class="card">
+						<img src="${response.url}" class="card-image" />
+					</div>
+					`;
+					resultNode.innerHTML = cardBlock;
+				})
+				.catch(() => {
+					console.log('error')
+				});
+		}
+});	
